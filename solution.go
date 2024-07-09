26b85a0e6c2aeec2b09c488f3cfaeabb8950d218
Justin <justin.li@optable.co>
2024-07-09 16:12:12 -0500

    page more

--- solution.go
+++ solution.go
@@ -1,33 +1,37 @@
 package main
 
-import "errors"
-
 func SearchString(mem *Memory) (string, error) {
-	// Read the first page
-	page, err := mem.ReadPage(0)
-	if err != nil {
-		return "", err
-	}
+	var (
+		pageN  int
+		window []byte
+	)
 
-	var window []byte
-	for _, p := range page {
-		b, err := mem.ReadAddress(int(p))
+	for {
+		// Read the first page
+		page, err := mem.ReadPage(pageN)
 		if err != nil {
 			return "", err
 		}
 
-		if len(window) == 5 && string(window) != "gc24{" {
-			// reset window
-			window = []byte{}
+		for _, p := range page {
+			b, err := mem.ReadAddress(int(p))
+			if err != nil {
+				return "", err
+			}
+
+			if len(window) == 5 && string(window) != "gc24{" {
+				// reset window
+				window = []byte{}
+			}
+
+			if len(window) > 5 && string(window[len(window)-1]) == "}" {
+				// end of flag
+				return string(window), nil
+			}
+
+			window = append(window, b)
 		}
 
-		if len(window) > 5 && string(window[len(window)-1]) == "}" {
-			// end of flag
-			return string(window), nil
-		}
-
-		window = append(window, b)
+		pageN += 1
 	}
-
-	return "", errors.New("flag not found")
 }
