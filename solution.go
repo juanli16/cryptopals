14e5a4fec11e39f39140a6dab423981e80208e74
Justin <justin.li@optable.co>
2024-07-09 16:42:33 -0500

    use semaphore

--- solution.go
+++ solution.go
@@ -1,42 +1,64 @@
 package main
 
+import (
+	"context"
+	"fmt"
+)
+
 func SearchString(mem *Memory) (string, error) {
 	var (
 		pageN  int
 		window []byte
+		sem    = semaphore.NewWeighted(4)
+		ctx    = context.Background()
+		res    string
 	)
 
-	for {
-		// Read the first page
-		page, err := mem.ReadPage(pageN)
-		if err != nil {
+	for pageN := 0; pageN < 1000; pageN++ {
+		if err := sem.Acquire(ctx, 1); err != nil {
 			return "", err
 		}
 
-		for _, p := range page {
-			b, err := mem.ReadAddress(int(p))
+		go func(pageN int) {
+			defer sem.Release(1)
+			// Read the first page
+			page, err := mem.ReadPage(pageN)
 			if err != nil {
-				return "", err
+				return
 			}
 
-			// testing
-			if len(window) == 4 {
-				return string(window), nil
+			for _, p := range page {
+				b, err := mem.ReadAddress(int(p))
+				if err != nil {
+					return
+				}
+
+				if len(window) == 5 && string(window) != "gc24{" {
+					// reset window
+					window = []byte{}
+				}
+
+				if len(window) > 5 && string(window[len(window)-1]) == "}" {
+					// end of flag
+					res = string(window)
+					return
+				}
+
+				window = append(window, b)
 			}
 
-			if len(window) == 5 && string(window) != "gc24{" {
-				// reset window
-				window = []byte{}
-			}
-
-			if len(window) > 5 && string(window[len(window)-1]) == "}" {
-				// end of flag
-				return string(window), nil
-			}
-
-			window = append(window, b)
-		}
-
-		pageN += 1
+			return
+		}(pageN)
 	}
+
+	// We block here until done.
+	if err := sem.Acquire(ctx, 4); err != nil {
+		fmt.Printf("Failed to acquire semaphore: %v", err)
+	}
+
+	if res != "" {
+		return res, nil
+	}
+
+	return "", fmt.Errorf("flag not found")
 }
