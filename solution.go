ab919fb00e368e2dc25e9bacfc5dfd143943b836
Justin <justin.li@optable.co>
2024-07-09 16:03:01 -0500

    add print

--- solution.go
+++ solution.go
@@ -9,15 +9,26 @@ func SearchString(mem *Memory) (string, error) {
 		return "", err
 	}
 
-	fmt.Println("page", page)
-
+	var window []byte
 	for _, p := range page {
 		b, err := mem.ReadAddress(int(p))
 		if err != nil {
 			return "", err
 		}
 
-		fmt.Println("address", p, "value", b)
+		if len(window) == 5 && string(window) != "gc24{" {
+			// reset window
+			window = []byte{}
+		}
+
+		if len(window) > 5 && string(window[len(window)-1]) == "}" {
+			// end of flag
+			fmt.Println("found ", string(window))
+			break
+		}
+
+		window = append(window, b)
+		fmt.Println(string(window))
 	}
 
 	return "hey", nil
